b774d38695534cdf261fb2338bcfc5bf1cdf3a63
x-na-ogata <x-na-ogata@KTEC-2112PC0119.local>
2026-08-17 17:50:07 +0900

    botton

--- hello.js
+++ hello.js
@@ -6,11 +6,22 @@ const port = gpioAccess.ports.get(26); // 26 番ポートを操作する
 
 await port.export("out"); // ポートを出力モードに設定
 
-// 無限ループ
-while (true) {
-  // 1秒間隔で LED が点滅します
-  await port.write(1); // LEDを点灯
-  await sleep(1000); // 1000 ms (1秒) 待機
-  await port.write(0); // LEDを消灯
-  await sleep(1000); // 1000 ms (1秒) 待機
-}
+// // 無限ループ
+// while (true) {
+//   // 1秒間隔で LED が点滅します
+//   await port.write(1); // LEDを点灯
+//   await sleep(1000); // 1000 ms (1秒) 待機
+//   await port.write(0); // LEDを消灯
+//   await sleep(1000); // 1000 ms (1秒) 待機
+// }
+
+const port2 = gpioAccess.ports.get(5);
+await port2.export("in");
+port2.onchange = async (e) => {
+  console.log(e.value);
+  if (e.value == 0) {
+    await port.write(1);
+  } else {
+    await port.write(0);
+  }
+};
